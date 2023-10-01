6ea09665cc1982a3020e39f43c350cede47e9e4a
johnbooze <johnbooze@gmail.com>
2023-10-01 05:19:00 -0700

    Update p.js

--- vue/p.js
+++ vue/p.js
@@ -1,9 +1,22 @@
 const { createApp } = Vue;
 
+class Post {
+    /** 
+     * @param {number} userId
+     * @param {number} id
+     * @param {string} title
+     * @param {string} body
+    */
+    constructor(userId, id, title, body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+}
+
 createApp({
   data() {
-    return {
-      message: 'Hello Vue!'
-    }
+    return new Post(2468, 1, "Title", "Body text");
   }
 }).mount('#app');
